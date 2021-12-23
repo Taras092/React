45bc7996ba576a9ad5128f6ab4fe38ec2d64a2ce
taras <taraslychynets@icloud.com>
2021-12-23 13:22:25 +0200

    First React App

---
+++ Lessons1/task1/index.js
@@ -0,0 +1,15 @@
+
+const rootElement = document.querySelector('#root');
+// const greetingElement = document.createElement('div');
+// greetingElement.classList.add('greeting');
+// greetingElement.textContent = "Hello, React!";
+
+// rootElement.append(greetingElement);
+
+const greetingElement = React.createElement(
+  'div',
+  { className: 'greeting' },
+  "Hello, React!"
+);
+
+ReactDOM.render(greetingElement, rootElement);
