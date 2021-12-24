3af2b4f36570d8558dc3b40725bfc9bbf2a5385e
taras <taraslychynets@icloud.com>
2021-12-24 18:39:24 +0200

    Функциональные компоненты

--- Lessons3/task1/src/Search.jsx
+++ Lessons3/task1/src/Search.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Search = (props) => {
+export default Search = (props) => {
   return (
     <div className="search">
     <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
