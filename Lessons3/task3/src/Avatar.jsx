2d7f45d7a73e5ae822e2dba9199eebcbec4afb89
taras <taraslychynets@icloud.com>
2021-12-31 16:00:57 +0200

    Композиция компонентов

---
+++ Lessons3/task3/src/Avatar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./comment.scss";
+
+const Avatar = (props) => {
+  return (
+    <img
+    className="avatar"
+    src={props.avatarUrl}
+    alt={props.name}
+  />
+  );
+}
+
+export default Avatar;
---
+++ Lessons3/task3/src/UserInfo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Avatar from "./Avatar";
+import "./comment.scss";
+
+const UserInfo = (props) => {
+  return (
+    <div className="user-info">
+      <Avatar 
+      avatarUrl={props.user.avatarUrl}
+      name={props.user.name}
+      />
+      <div className="user-info__name">{props.user.name}</div>
+    </div>
+  );
+};
+
+export default UserInfo;
