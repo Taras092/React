3c453bc6c29c49b48ae7e53c192c412a7d24b0a6
taras <taraslychynets@icloud.com>
2022-01-03 16:32:58 +0200

    Profile

--- Lessons3/task5/src/Profile.jsx
+++ Lessons3/task5/src/Profile.jsx
@@ -8,10 +8,10 @@ const Profile = (props) => {
   return (
     <>
     <div className="profile__name">
-    {`${props.user.firstName} ${props.user.lastName}`}
+    {`${props.userData.firstName} ${props.userData.lastName}`}
   </div>
   <div className="profile__birth">
-    {`Was born ${formatDate(props.user.birthDate)} in ${props.user.birthPlace}`}
+    {`Was born ${formatDate(props.userData.birthDate)} in ${props.userData.birthPlace}`}
   </div>
   </>
   );
