a363d166226b11198aa0a6735b67ac00185e42fe
taras <taraslychynets@icloud.com>
2022-01-05 01:13:07 +0200

    Мировые часы

--- Lessons4/task2/src/App.jsx
+++ Lessons4/task2/src/App.jsx
@@ -4,8 +4,8 @@ import Clock  from "./Clock";
 const App = () => {
   return (
     <>
-      <Clock location="New York" offset={2} interval={1000}/>
-      <Clock location="Kyiv" offset={-5} interval={1000}/>
+      <Clock location="New York" offset={-5} interval={1000}/>
+      <Clock location="Kyiv" offset={2} interval={1000}/>
       <Clock location="London" offset={0} interval={1000}/>
     </>
   );
