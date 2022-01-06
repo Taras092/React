63aaad1d62fbc7d4e84e578daca357cb7b736a1d
taras <taraslychynets@icloud.com>
2022-01-06 13:51:51 +0200

    Переключатель

--- Lessons5/task5/src/Toggler.jsx
+++ Lessons5/task5/src/Toggler.jsx
@@ -2,18 +2,30 @@ import React, { Component } from "react";
 import "./index.scss";
 
 class Toggler extends Component {
-  onButton(e) {
-    if (e.target.textContent === "Off") {
-      e.target.textContent = "On";
-    } else if(e.target.textContent === "On") {
-      e.target.textContent = "Off"
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: "Off",
+    };
+  }
+  // onButton(e) {
+  //   if (e.target.textContent === "Off") {
+  //     e.target.textContent = "On";
+  //   } else if(e.target.textContent === "On") {
+  //     e.target.textContent = "Off"
+  //   }
+  // }
+
+  setButton() {
+    this.setState({
+      active: this.state.active ? 'On' : 'Off',
+    })
   }
 
   render() {
     return (
       <div>
-        <button className="toggler" onClick={this.onButton}>Off</button>
+        <button className="toggler" onClick={() => this.setButton()}>{this.state.active}</button>
       </div>
     );
   }
