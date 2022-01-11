0c07a4d6306c4568736f1d3c947dbabc05c27aa0
taras <taraslychynets@icloud.com>
2022-01-12 00:26:52 +0200

    Transactions list

---
+++ Lessons7/task3/src/Transaction.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import moment from "moment";
+
+const formatDateDay = (date) => moment(date).format("ll");
+const formatDateTime = (date) => moment(date).format("LT");
+
+const Transaction = ({ from, to, amount, rate, time }) => {
+  return (
+    <li className="transaction">
+      <span className="transaction__date">
+        {formatDateDay(time).split(",").slice(0, 1).reverse()}
+      </span>
+      <span className="transaction__time">
+        {formatDateTime(time).split(" ").slice(0, 1)}
+      </span>
+      <span className="transaction__assets">
+        {from} → {to}
+      </span>
+      <span className="transaction__rate">
+        {new Intl.NumberFormat().format(rate)}
+      </span>
+      <span className="transaction__amount">
+        {new Intl.NumberFormat("en-GB", { maximumSignificantDigits: 3 }).format(
+          amount
+        )}
+      </span>
+    </li>
+  );
+};
+
+export default Transaction;
---
+++ Lessons7/task3/src/TransactionsList.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Transaction from "./Transaction";
+
+class TransactionsList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    let transactionsList;
+    transactionsList = this.props.transactions.slice();
+    return (
+      <ul className="transactions">
+        {transactionsList.map((transactionList) => (
+          <Transaction key={transactionList.id} {...transactionList} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TransactionsList;
