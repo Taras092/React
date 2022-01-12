c7749d2f676b171ee478699843bc4d3f3874c30c
taras <taraslychynets@icloud.com>
2022-01-12 13:26:13 +0200

    Методы жизненного цикла

--- Lessons8/task1/src/Life.jsx
+++ Lessons8/task1/src/Life.jsx
@@ -18,7 +18,6 @@ class Life extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // console.log(prevProps);
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 
