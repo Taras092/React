461b714da34645dc4a880ece4b701491774bbad9
taras <taraslychynets@icloud.com>
2022-01-12 17:54:20 +0200

    Подгрузка данных с сервера

---
+++ Lessons8/task4/src/User.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class User extends Component {
+render() {
+  return (
+    <div class="user">
+  <img
+    alt="User Avatar"
+    src="https://avatars3.githubusercontent.com/u/69631?v=4"
+    class="user__avatar"
+  />
+  <div class="user__info">
+    <span class="user__name">Facebook</span>
+    <span class="user__location">Menlo Park, California</span>
+  </div>
+</div>
+  );
+}
+}
+
+export default User;
