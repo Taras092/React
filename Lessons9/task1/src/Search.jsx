43fe034c60457ef59e0515290a7580d049ebf6ec
taras <taraslychynets@icloud.com>
2022-01-14 00:23:57 +0200

    Controlled component

--- Lessons9/task1/src/Search.jsx
+++ Lessons9/task1/src/Search.jsx
@@ -26,15 +26,19 @@ class Search extends Component {
 
   render() {
     return (
-      <form className="search" onSubmit={this.search}>
+      <form className="search">
         <input
           type="text"
           onChange={this.handleChange}
           value={this.state.value}
           className="search__input"
         />
-        <button className="search__button" 
-        type="submit">Search</button>
+        <button 
+        onClick={this.search} 
+        className="search__button" 
+        type="submit">
+          Search
+        </button>
       </form>
     );
   }
