12274578940c1c16f88de86a89eb189794ae24b0
taras <taraslychynets@icloud.com>
2022-01-17 16:58:21 +0200

    Список товаров

---
+++ Lessons10/task2/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import ShoppingCart from './ShopingCart';
+import Profile from './Profile';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userData: {
+        firstName: 'Tom',
+        lastName: 'Ford',
+      },
+    };
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      userData: {
+        ...this.state.userData,
+        [name]: value,
+      },
+    });
+  };
+
+  render() {
+    const { userData } = this.state;
+    return (
+      <div className="page">
+        <h1 className="title">
+         {` Hello, ${userData.firstName} ${userData.lastName}`}
+        </h1>
+        <main className="content">
+          <ShoppingCart userData={this.state.userData}/>
+          <Profile userData={this.state.userData} handleChange={this.handleChange} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
