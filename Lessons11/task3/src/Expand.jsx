6e9d398b945d8b35ce1a6778f93ec8fc65c5d338
taras <taraslychynets@icloud.com>
2022-01-19 17:36:48 +0200

    Expanded Component

--- Lessons11/task3/src/Expand.jsx
+++ Lessons11/task3/src/Expand.jsx
@@ -12,19 +12,21 @@ class Expand extends Component {
   };
 
   render() {
+  const { isOpen } = this.state;
+  const { title, children } = this.props;
     return (
       <div className="expand border">
         <div className="expand__header">
-          <span className="expand__title">{this.props.title}</span>
+          <span className="expand__title">{title}</span>
           <button className="expand__toggle-btn" onClick={this.toggleContent}>
-            {this.state.isOpen ? (
+            {isOpen ? (
               <i className="fas fa-chevron-up"></i>
             ) : (
               <i className="fas fa-chevron-down"></i>
             )}
           </button>
         </div>
-        {this.state.isOpen && <div className="expand__content">{this.props.children}</div>}
+        {isOpen && <div className="expand__content">{children}</div>}
       </div>
     );
   }
