27234998989fb6c76bb763e933bd4e40048a35a9
taras <taraslychynets@icloud.com>
2022-01-20 15:46:41 +0200

    Todo List

--- Lessons12/task1/src/TasksList.jsx
+++ Lessons12/task1/src/TasksList.jsx
@@ -1,67 +1,22 @@
 import React, { Component } from 'react';
 import Task from './Task';
-import CreateTaskInput from './CreateTaskInput';
-import { createTask, fetchTasksList, updateTask, deleteTask } from './tasksGateway';
 
-class TasksList extends Component {
-  state = {
-    tasks: [],
-  };
-
-  componentDidMount() {
-    this.fetchTasks();
-  }
-
-  fetchTasks = () => {
-    fetchTasksList().then(tasksList =>
-      this.setState({
-        tasks: tasksList,
-      }),
-    );
-  };
-
-  onCreate = text => {
-    const newTask = {
-      text,
-      done: false,
-      createDate: new Date().toISOString(),
-    };
-
-    createTask(newTask).then(() => this.fetchTasks());
-  };
-
-  handleTaskStatusChange = id => {
-    const { done, text } = this.state.tasks.find(task => task.id === id);
-    const updatedTask = {
-      text,
-      done: !done,
-    };
-    updateTask(id, updatedTask).then(() => this.fetchTasks());
-  };
-
-  handleTaskDelete = id => {
-    deleteTask(id).then(() => this.fetchTasks());
-  };
-
-  render() {
-    const sortedList = this.state.tasks.slice().sort((a, b) => a.done - b.done);
+const TasksList = ({ tasks, handleTaskStatusChange, handleTaskDelete }) => {
+    const sortedList = tasks.slice().sort((a, b) => a.done - b.done);
 
     return (
-      <main className="todo-list">
-        <CreateTaskInput onCreate={this.onCreate} />
         <ul className="list">
           {sortedList.map(task => (
             <Task
               key={task.id}
               {...task}
-              onChange={this.handleTaskStatusChange}
-              onDelete={this.handleTaskDelete}
+              onChange={handleTaskStatusChange}
+              onDelete={handleTaskDelete}
             />
           ))}
         </ul>
-      </main>
     );
   }
-}
+
 
 export default TasksList;
--- Lessons12/task1/src/TodoList.jsx
+++ Lessons12/task1/src/TodoList.jsx
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { Component } from 'react';
 import TasksList from './TasksList';
+import CreateTaskInput from './CreateTaskInput';
+import { createTask, fetchTasksList, updateTask, deleteTask } from './tasksGateway';
 
-const TodoList = () => {
-  return (
-    <>
-    <h1 className='title'>Todo List</h1>
-    <TasksList />
-    </>
-  );
+class TodoList extends Component {
+  state = {
+    tasks: [],
+  };
+
+  componentDidMount() {
+    this.fetchTasks();
+  }
+
+  fetchTasks = () => {
+    fetchTasksList().then(tasksList =>
+      this.setState({
+        tasks: tasksList,
+      }),
+    );
+  };
+
+  onCreate = text => {
+    const newTask = {
+      text,
+      done: false,
+      createDate: new Date().toISOString(),
+    };
+
+    createTask(newTask).then(() => this.fetchTasks());
+  };
+
+  handleTaskStatusChange = id => {
+    const { done, text } = this.state.tasks.find(task => task.id === id);
+    const updatedTask = {
+      text,
+      done: !done,
+    };
+    updateTask(id, updatedTask).then(() => this.fetchTasks());
+  };
+
+  handleTaskDelete = id => {
+    deleteTask(id).then(() => this.fetchTasks());
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className="title">Todo List</h1>
+        <main className="todo-list">
+          <CreateTaskInput onCreate={this.onCreate} />
+          <TasksList
+            tasks={this.state.tasks}
+            handleTaskStatusChange={this.handleTaskStatusChange}
+            handleTaskDelete={this.handleTaskDelete}
+          />
+        </main>
+      </>
+    );
+  }
 }
 
 export default TodoList;
