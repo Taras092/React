bf76e323410096fb10d38a3b4ac8e52a9c476b75
taras <taraslychynets@icloud.com>
2022-01-20 16:13:24 +0200

    Todo List

--- Lessons12/task1/src/CreateTaskInput.jsx
+++ Lessons12/task1/src/CreateTaskInput.jsx
@@ -25,7 +25,7 @@ class CreateTaskInput extends Component {
           onChange={this.handleChange}
           className="create-task__input"
         />
-        <button className="btn create-task_btn" onClick={this.handleTaskCreate}>
+        <button className="btn create-task__btn" onClick={this.handleTaskCreate}>
           Create
         </button>
       </div>
