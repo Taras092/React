f2ffeca097b6ce6c54c48c58389165aaf65b053b
taras <taraslychynets@icloud.com>
2022-01-24 20:09:50 +0200

    Подписка на события - React Hooks

--- Lessons14/task4/src/ConnectionStatus.jsx
+++ Lessons14/task4/src/ConnectionStatus.jsx
@@ -1,5 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
+// const ConnectionStatus = () => {
+//   const [isOnline, setPageStatus] = useState(true);
+
+//   useEffect(() => {
+//     const setOnlineStatus = e => {
+//       setPageStatus(navigator.onLine);
+//     };
+//     const setOfflineStatus = e => {
+//       setPageStatus(navigator.onLine);
+//     }
+//     window.addEventListener('online', setOnlineStatus);
+//     window.addEventListener('offline', setOfflineStatus);
+//         if (navigator.onLine) {
+//           setOnlineStatus();
+//         } else {
+//           setOfflineStatus();
+//         }
+
+//     return () => {
+//       window.removeEventListener('online', setOnlineStatus);
+//       window.removeEventListener('offline', setOfflineStatus);
+//     };
+//   }, []);
+
+//   if (isOnline) {
+//     return <div className="status">online</div>;
+//   }
+//   return <div className="status status_offline">offline</div>;
+// };
+
 const ConnectionStatus = () => {
   const [isOnline, setPageStatus] = useState(true);
 
@@ -7,20 +37,14 @@ const ConnectionStatus = () => {
     const setOnlineStatus = e => {
       setPageStatus(navigator.onLine);
     };
-    const setOfflineStatus = e => {
-      setPageStatus(navigator.onLine);
-    }
+
     window.addEventListener('online', setOnlineStatus);
-    window.addEventListener('offline', setOfflineStatus);
-        if (navigator.onLine) {
-          setOnlineStatus();
-        } else {
-          setOfflineStatus();
-        }
+    window.addEventListener('offline', setOnlineStatus);
+    setOnlineStatus();
 
     return () => {
       window.removeEventListener('online', setOnlineStatus);
-      window.removeEventListener('offline', setOfflineStatus);
+      window.removeEventListener('offline', setOnlineStatus);
     };
   }, []);
 
