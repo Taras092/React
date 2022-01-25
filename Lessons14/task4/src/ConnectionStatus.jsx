c950a2542ea8e5b7c1853929e18cdf8b54d75d77
taras <taraslychynets@icloud.com>
2022-01-25 10:02:52 +0200

    Подписка на события - React Hooks

--- Lessons14/task4/src/ConnectionStatus.jsx
+++ Lessons14/task4/src/ConnectionStatus.jsx
@@ -1,65 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
-// const ConnectionStatus = () => {
-//   const [isOnline, setPageStatus] = useState(true);
-
-//   useEffect(() => {
-//     const setOnlineStatus = e => {
-//       setPageStatus(navigator.onLine);
-//     };
-//     const setOfflineStatus = e => {
-//       setPageStatus(navigator.onLine);
-//     }
-//     window.addEventListener('online', setOnlineStatus);
-//     window.addEventListener('offline', setOfflineStatus);
-//         if (navigator.onLine) {
-//           setOnlineStatus();
-//         } else {
-//           setOfflineStatus();
-//         }
-
-//     return () => {
-//       window.removeEventListener('online', setOnlineStatus);
-//       window.removeEventListener('offline', setOfflineStatus);
-//     };
-//   }, []);
-
-//   if (isOnline) {
-//     return <div className="status">online</div>;
-//   }
-//   return <div className="status status_offline">offline</div>;
-// };
-
 const ConnectionStatus = () => {
-  const [isOnline, setPageStatus] = useState(true);
+  const [status, setStatus] = useState('online');
 
   useEffect(() => {
-    const setOnlineStatus = e => {
-      setPageStatus(navigator.onLine);
+    const setOnlineStatus = () => {
+      setStatus('online');
     };
+    const setOfflineStatus = () => {
+      setStatus('offline');
+    }
     window.addEventListener('online', setOnlineStatus);
-      setOnlineStatus();
-    
+    window.addEventListener('offline', setOfflineStatus);
 
     return () => {
       window.removeEventListener('online', setOnlineStatus);
+      window.removeEventListener('offline', setOfflineStatus);
     };
   }, []);
 
-    useEffect(() => {
-      const setOfflineStatus = e => {
-        setPageStatus(navigator.onLine);
-      };
-      window.addEventListener('offline', setOfflineStatus);
-        setOfflineStatus();
-
-
-      return () => {
-        window.removeEventListener('offline', setOfflineStatus);
-      };
-    }, []);
-
-  if (isOnline) {
+  if (status === 'online') {
     return <div className="status">online</div>;
   }
   return <div className="status status_offline">offline</div>;
